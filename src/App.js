b2f174c8e0f8360d6c9c01fe3951d8f9217c7a26
Dileesha Nuwansara <124884306+DileeshaNuwansara@users.noreply.github.com>
2024-02-14 12:32:30 +0530

    ckeck error again

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import StudentSignup from "./formcontrol/student/studentRegister";
 import StudentDashBoard from "./formcontrol/student/StudentDashBoard1";
 import MentorDashBoard from "./formcontrol/mentor/MentorDashboard";
 import ForgotPassword from "./formcontrol/forgotPassword";
-import PersonalityTesting from "./formcontrol/student/PersonailtyTesting";
-import Mentor from "./formcontrol/student/Mentor";
 import PersonalityTypes from "./formcontrol/student/PersonalityTypes";
+import Mentor from "./formcontrol/student/Mentor";
+import PersonalityTest from "./formcontrol/student/PersonalityTest";
 import StressFree from "./formcontrol/student/StressFree";
 import Resources from "./formcontrol/student/Resources";
 import GPA from "./formcontrol/student/GPA";
@@ -47,9 +47,8 @@ function App() {
             <Route path="/Mentor-Dashboard" element={<MentorDashBoard />} />
             <Route path="/Student-GPA-Calculator" element={<GPA />} />
             <Route path="/Student-Mentor" element={<Mentor />} />
-            <Route
-              path="/Student-Personality-Testing" element={<PersonalityTesting />}
-            />
+            <Route path="/Student-Personality-Test" element={<PersonalityTest />} />
+            <Route path="/Student-Personality-Types" element={<PersonalityTypes />} />
             <Route path="/Student-Stress-Free" element={<StressFree />} />
             <Route path="/Student-Resources" element={<Resources />} />
             <Route
@@ -57,8 +56,6 @@ function App() {
               element={<StudentProfileView />}
             />
             <Route path="/Student-Profile-Update" element={<UpdateProfile />} />
-
-
             <Route path="/Mentor-Personality-Types" element={<PersonalityGuidance />} />
             <Route path="/Mentor-My-Studnets" element={<MyStudents />} />
           </Routes>
