62780592508524c12c6c4f6fbe4e928f7de3695f
yasurirashmika <iyasuri.rash@gmail.com>
2024-04-06 10:05:38 +0530

    git commit -m "Personality Types layout changed"

--- src/formcontrol/Admin/AdminDashboard.jsx
+++ src/formcontrol/Admin/AdminDashboard.jsx
@@ -4,6 +4,8 @@ import Layout from '../../content/NavbarSidenavLayout';
 const drawerWidth = 240;
 
 export default function AdminDashboard() {
+
+  window.localStorage.setItem("User", "Admin");
   return (
     <Layout>
         <h1>Admin</h1>
