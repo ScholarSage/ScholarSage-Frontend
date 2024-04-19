c2af95860acfe0540e8fc9edd58a6717c13c25dc
Dilshan Wickramasinghe <dilshanmw717@gmail.com>
2024-04-19 23:06:57 +0530

    new

--- src/formcontrol/mentor/mentorAppointment.js
+++ src/formcontrol/mentor/mentorAppointment.js
@@ -4,18 +4,23 @@ import { useDispatch } from "react-redux";
 import axios from "axios";
 import { Table } from "antd";
 import moment from "moment";
+import Layout from "../../content/NavbarSidenavLayout";
+import { DataGrid } from "@mui/x-data-grid";
+import { useNavigate } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
 
 function MentorAppointments() {
   const [appointments, setAppointments] = useState([]);
   const [userData, setUserData] = useState("");
+  const navigate = useNavigate();
 
   //const [time, setTime] = useState();
   const dispatch = useDispatch();
 
-  const getAppointmentsData = async () => {
+  const getData = async () => {
     try {
-      const response = await axios.get(
-        "http://localhost:8081/get-appointments-mentor",
+      const response = await axios.post(
+        "http://localhost:8081/userData",
         {},
         {
           headers: {
@@ -23,95 +28,111 @@ function MentorAppointments() {
           },
         }
       );
-      console.log("response.data.data: ", response.data.data);
-      setAppointments(response.data.data);
+      console.log(response.data);
+      setUserData(response.data.data);
+      return response.data.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getAppointmentsData = async (mentorID) => {
+    console.log(mentorID);
+    console.log("Call API");
+    try {
+      const response = await axios.post(
+        "http://localhost:8081/get-appointments-mentor",
+        {
+          mentorID,
+        }
+      );
+      console.log(response.data);
+      setAppointments(response.data.appointments);
+      console.log(appointments);
     } catch (error) {
       console.log(error);
     }
   };
   useEffect(() => {
-    getAppointmentsData();
+    const fetchData = async () => {
+      const userData = await getData();
+      if (userData) {
+        getAppointmentsData(userData.mentorid);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  const changeAppointmentStatus = async (record, status) => {
-    try {
-      const response = await axios.post(
-        "/api/mentor/change-appointment-status",
-        {
-          appointmentId: record._id,
-          status: status,
-        }
-      );
-
-      if (response.data.success) {
-        toast.success(response.data.message);
-        getAppointmentsData();
-      }
-    } catch (error) {
-      toast.error("error changing appointment status");
-    }
-  };
-
   const columns = [
-    {
-      title: "Id",
-      dataIndex: "_id",
-    },
-    {
-      title: "Student",
-      dataIndex: "name",
-      render: (text, record) => (
-        <span>
-          {record.studentInfo.fname}
-          {record.studentInfo.lname}
-        </span>
-      ),
-    },
-    {
-      title: "Email",
-      dataIndex: "email",
-    },
-
-    {
-      title: "Date & Time",
-      dataIndex: "email",
-      render: (text, record) => (
-        <span>
-          {moment(record.date).format("DD-MM-YYYY")}
-          {moment(record.time).format("HH:mm")}
-        </span>
-      ),
-    },
-    {
-      title: "Status",
-      dataIndex: "status",
-    },
-    {
-      title: "Actions",
-      dataIndex: "actions",
-      render: (text, record) => (
-        <div>
-          {record.status === "pending" && (
-            <div>
-              <h1 onClick={() => changeAppointmentStatus(record, "approved")}>
-                Approve
-              </h1>
-
-              <h1 onClick={() => changeAppointmentStatus(record, "rejected")}>
-                Reject
-              </h1>
-            </div>
-          )}
-        </div>
-      ),
-    },
+    { field: "_id", headerName: "ID", width: 200 },
+    { field: "scnumber", headerName: "SC Number", width: 200 },
+    { field: "mentorid", headerName: "Mentor ID", width: 200 },
+    { field: "date", headerName: "Date", width: 200 },
+    { field: "time", headerName: "Time", width: 200 },
+    { field: "status", headerName: "Status", width: 200 },
   ];
 
   return (
-    <div>
-      <h1>Appointments</h1>
-      <Table columns={columns} dataSource={appointments} />
-    </div>
+    <Layout>
+      <Box
+        sx={{
+          height: "100%",
+          width: "100%",
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: "", // Change header background color
+            borderBottom: "",
+            color: "",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: "", // Change row background color
+            color: "",
+          },
+          "& .MuiDataGrid-row:hover": {
+            backgroundColor: "", // Change hover color of rows
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: "", // Change footer background color
+            color: "", // Change footer font color
+          },
+          "& .MuiDataGrid-footerContainer .MuiTypography-root": {
+            // Change font color of footer text
+            color: "",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            height: 60, // Set the height of the title bar
+            backgroundColor: "#720dba", // Change title bar background color
+            color: "white", // Change title bar font color
+            paddingLeft: 2,
+            justifyContent: "center",
+          }}
+        >
+          <Typography variant="h6">Mentor Requests</Typography>
+        </Box>
+        <DataGrid
+          rows={appointments}
+          columns={columns}
+          sx={{
+            height: 600,
+          }}
+          onRowClick={(params) => {
+            navigate(`/MentorDertails/${params.row.mentorid}`);
+          }}
+        />
+      </Box>
+    </Layout>
   );
 }
 
